f1ecbc747c2b88cac264b0c5bf3d4e6657dbe4d1
haghighataliasghar12-cloud <haghighataliasghar12@gmail.com>
2026-06-26 08:25:42 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Park Residence Loaded");
+
+    const progress = document.querySelector(".progress-bar");
+    const percent = document.getElementById("percent");
+
+    let value = 35;
+
+    if(progress){
+        progress.style.width = value + "%";
+    }
+
+    if(percent){
+        percent.innerText = value + "%";
+    }
+});
